1756cfe3e50a42c46984a82ec2db8789bee375c3
J Parks <jparks87@gmail.com>
2022-03-24 10:52:19 +0000

    first commit

---
+++ resources/js/Components/Notification.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    props: ['message', 'type'],
+
+    mounted() {
+        this.$toasts.push({
+            message: this.message,
+            type: this.type,
+            position: 'top-right',
+        });
+    },
+};
+</script>
